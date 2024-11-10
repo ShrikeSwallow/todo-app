6579368f80a3a19ea41702c6578e1f09f2cf725b
Maciej Jaskolka <maciej.jaskolka@gmail.com>
2024-11-10 20:53:31 +0000

    Implement Project Manager class and methods

--- src/objectManagers.js
+++ src/objectManagers.js
@@ -1,5 +1,21 @@
 import { Project, ToDo } from "./classes.js";
-class ProjectManager {}
+class ProjectManager {
+  constructor() {
+    this.projects = [new Project("Default", "")];
+    this.completedProjects = [];
+  }
+  addProject(project) {
+    this.projects.push(project);
+  }
+  completeProject(project) {
+    project.complete();
+    this.completedProjects.push(this.projects.splice(indexOf(project), 1));
+  }
+  deleteProject(project) {
+    this.projects.splice(indexOf(project), 1);
+    project.delete();
+  }
+}
 
 /*const addNewProject = (project) => {
   return new Project(project);
