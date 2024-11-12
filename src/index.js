c28a37a74356fdefe4c9a49baa5735b742710ec4
Maciej Jaskolka <maciej.jaskolka@gmail.com>
2024-11-12 04:24:48 +0000

    Implement fixed HTML elements into template

--- src/index.js
+++ src/index.js
@@ -4,7 +4,8 @@ import { ProjectManager } from "./objectManagers.js";
 import { Project, ToDo } from "./classes";
 
 console.log("Loading succesful");
-const PM = new ProjectManager();
+
+/*const PM = new ProjectManager();
 PM.addProject(new Project("testname", "testdate"));
 const testTodo = new ToDo(
   "TodoTest",
@@ -20,4 +21,4 @@ PM.moveToDo(testTodo, PM.projects[0], PM.projects[1]);
 console.log("After moving todo");
 PM.showAllProjects();
 PM.removeToDo(testTodo, PM.projects[1]);
-PM.showAllProjects();
+PM.showAllProjects();*/
