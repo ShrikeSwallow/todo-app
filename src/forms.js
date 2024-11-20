6dd025af7aad3f8b79e0b239e45c3dab88e54dd8
Maciej Jaskolka <maciej.jaskolka@gmail.com>
2024-11-20 03:44:03 +0000

    Add function to create form for ToDo edition

--- src/forms.js
+++ src/forms.js
@@ -235,29 +235,30 @@ export class Forms {
     form.appendChild(fieldset);
     main.appendChild(form);
   }
-  editProject(project) {
+  editToDo(todo) {
+    const main = document.querySelector("main");
     const form = document.createElement("form");
-    form.classList.add("form-edit-project", "hidden");
+    form.classList.add("form-edit-todo", "hidden");
 
     const fieldset = document.createElement("fieldset");
     const legend = document.createElement("legend");
-    legend.textContent = `Edit ${project.name} details`;
+    legend.textContent = "Edit ToDo";
     fieldset.appendChild(legend);
 
-    //create project name div and its elements
+    //create TODO name div and its elements
     const name = document.createElement("div");
     name.classList.add("form-field");
 
     const nameInput = document.createElement("input");
     nameInput.type = "text";
-    nameInput.id = "project-name";
-    nameInput.name = "project-name";
-    nameInput.placeholder = project.name;
+    nameInput.id = "todo-name";
+    nameInput.name = "todo-name";
+    nameInput.placeholder = `${todo.name}`;
     nameInput.required = true;
 
     const nameLabel = document.createElement("label");
     nameLabel.setAttribute("for", nameInput.id);
-    nameLabel.textContent = "Project Name";
+    nameLabel.textContent = "ToDo Name";
 
     name.appendChild(nameLabel);
     name.appendChild(nameInput);
@@ -266,37 +267,115 @@ export class Forms {
 
     //create due date div and its elements
     const dueDate = document.createElement("div");
-    name.classList.add("form-field");
+    dueDate.classList.add("form-field", "date-field");
 
     const dueDateInput = document.createElement("input");
-    dueDateInput.type = "date";
-    dueDateInput.id = "project-due-date";
-    dueDateInput.name = "project-due-date";
+    dueDateInput.type = "text";
+    dueDateInput.id = "todo-due-date";
+    dueDateInput.name = "todo-due-date";
+    dueDateInput.placeholder = `${todo.dueDate}`;
     dueDateInput.required = true;
 
     const dueDateLabel = document.createElement("label");
     dueDateLabel.setAttribute("for", dueDateInput.id);
-    dueDateLabel.textContent = "Project Name";
+    dueDateLabel.textContent = "Due Date";
 
     dueDate.appendChild(dueDateLabel);
     dueDate.appendChild(dueDateInput);
     fieldset.appendChild(dueDate);
-    //end of project due date section
+    dueDateInput.addEventListener("mouseover", (event) => {
+      console.log("Mouse over date");
+      this.convertTextToDate(event.currentTarget);
+    });
+    dueDateInput.addEventListener("blur", (event) => {
+      console.log("Mouse outside date");
+      this.convertDateToText(event.currentTarget);
+      console.log(dueDateInput.value);
+    });
+    //end of todo due date section
+
+    //create priority div and its elements
+    const priority = document.createElement("div");
+    priority.classList.add("form-field", "priority-field");
+
+    const priorityDropdown = document.createElement("select");
+    priorityDropdown.title = "todo-priority";
+    priorityDropdown.name = "todo-priority";
+    priorityDropdown.id = "todo-priority";
+    const priorities = ["Low", "Medium", "High"];
+    priorities.forEach((priority) => {
+      const option = document.createElement("option");
+      option.value = priority.toLowerCase();
+      option.textContent = priority;
+      if (todo.priority === option.value) {
+        option.selected = "selected";
+      }
+      priorityDropdown.appendChild(option);
+    });
+    const priorityLabel = document.createElement("label");
+    priorityLabel.setAttribute("for", priorityDropdown.id);
+    priorityLabel.textContent = "Priority:";
+
+    priority.appendChild(priorityLabel);
+    priority.appendChild(priorityDropdown);
+    fieldset.appendChild(priority);
+    //end of priority section;
+
+    //create TODO description div and its elements
+    const description = document.createElement("div");
+    description.classList.add("form-field");
+
+    const descriptionInput = document.createElement("input");
+    descriptionInput.type = "text";
+    descriptionInput.id = "todo-description";
+    descriptionInput.name = "todo-description";
+    descriptionInput.placeholder = `${todo.description}`;
+
+    const descriptionLabel = document.createElement("label");
+    descriptionLabel.setAttribute("for", descriptionInput.id);
+    descriptionLabel.textContent = "Description";
+
+    description.appendChild(descriptionLabel);
+    description.appendChild(descriptionInput);
+    fieldset.appendChild(description);
+    //end of description section
+
+    //create TODO notes div and its elements
+    const notes = document.createElement("div");
+    notes.classList.add("form-field");
+
+    const notesArea = document.createElement("textarea");
+    notesArea.id = "todo-notes";
+    notesArea.name = "todo-notes";
+    notesArea.placeholder = `${todo.notes}`;
+    notesArea.rows = "4";
+
+    const notesLabel = document.createElement("label");
+    notesLabel.setAttribute("for", notesArea.id);
+    notesLabel.textContent = "Notes";
+
+    notes.appendChild(notesLabel);
+    notes.appendChild(notesArea);
+    fieldset.appendChild(notes);
+    //end of notes section
 
     //create buttons
+    const buttons = document.createElement("div");
+    buttons.classList.add("form-field", "form-buttons");
     const okBtn = document.createElement("button");
     okBtn.classList.add("form-add-btn");
     okBtn.type = "submit";
     okBtn.textContent = "Ok";
-    fieldset.appendChild(okBtn);
+    buttons.appendChild(okBtn);
 
     const cancelBtn = document.createElement("button");
     cancelBtn.classList.add("form-cancel-btn");
-    cancelBtn.type = "button";
+    cancelBtn.type = "reset";
     cancelBtn.textContent = "Cancel";
-    fieldset.appendChild(cancelBtn);
+    buttons.appendChild(cancelBtn);
+    fieldset.appendChild(buttons);
 
     form.appendChild(fieldset);
-    this.forms.appendChild(form);
+    main.appendChild(form);
   }
 }
