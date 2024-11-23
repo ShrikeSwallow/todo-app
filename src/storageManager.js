5372be20670f47da8a88776f55d0d5584e218f55
Maciej Jaskolka <maciej.jaskolka@gmail.com>
2024-11-23 06:58:23 +0000

    Add storageManager class to store app's data in localStorage

---
+++ src/storageManager.js
@@ -0,0 +1,37 @@
+import { ProjectManager } from "./objectManagers";
+
+export class StorageManager {
+  constructor() {
+    if (StorageManager.instance) {
+      return StorageManager.instance;
+    }
+    StorageManager.instance = this;
+    this.PM = new ProjectManager();
+  }
+
+  storageAvailable(type) {
+    let storage;
+    try {
+      storage = window[type];
+      const x = "__storage_test__";
+      storage.setItem(x, x);
+      storage.removeItem(x);
+      return true;
+    } catch (e) {
+      return (
+        e instanceof DOMException &&
+        e.name === "QuotaExceededError" &&
+        // acknowledge QuotaExceededError only if there's something already stored
+        storage &&
+        storage.length !== 0
+      );
+    }
+  }
+
+  loadStoredProjects() {
+    const storedProjects = JSON.parse(localStorage.getItem("projects"));
+  }
+  updateStoredProjects() {
+    localStorage.setItem("projects", JSON.stringify(this.PM.projects));
+  }
+}
